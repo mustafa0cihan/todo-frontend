9ce65afb5e7f4290ae4d800581080bf96bef4f45
Mustafa Cihan <mustafa0cihan@gmail.com>
2024-10-02 21:04:52 +0200

    deploy-2

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">Todo List App</h1>
+      <h1 className="text-4xl font-bold mb-4">Todo List</h1>
       <div className="mb-4">
         <input
           value={newTodo}
